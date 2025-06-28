201dc025f027f5626e7048776b7bf33afd26baa9
Artem <artsefimov@gmail.com>
2025-06-28 23:15:56 +0300

    add filter and pagination

--- app_run/views.py
+++ app_run/views.py
@@ -1,7 +1,8 @@
 from django.forms.models import model_to_dict
-from rest_framework import filters, status
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from django.conf import settings
 from rest_framework.views import APIView
@@ -10,6 +11,8 @@ from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from app_run.models import Run
 from .serializers import RunSerializer, UserSerializer
 from django.contrib.auth.models import User
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter, SearchFilter
 
 
 @api_view(["GET"])
@@ -23,15 +26,24 @@ def company_info(request):
     )
 
 
+class RunPagination(PageNumberPagination):
+    page_size_query_param = "size"
+    max_page_size = 10
+
+
 class RunViewSet(ModelViewSet):
     queryset = Run.objects.select_related("athlete").all()
     serializer_class = RunSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filter_fields = ["status", "athlete"]
+    ordering_fields = ["created_at"]
+    pagination_class = RunPagination
 
 
 class UserViewSet(ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    filter_backends = (filters.SearchFilter,)
+    filter_backends = (SearchFilter,)
     search_fields = ("first_name", "last_name")
 
     def get_queryset(self):
