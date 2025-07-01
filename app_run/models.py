5d7463198ee309081d80d4fa32ba190313a5c2ae
artsefimov <artsefimov@gmail.com>
2025-07-01 07:56:10 +0300

    fix validators

--- app_run/models.py
+++ app_run/models.py
@@ -18,4 +18,4 @@ class Run(models.Model):
 class AthleteInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     goals = models.TextField(blank=True, default="")
-    weight = models.IntegerField(blank=True, default=0, validators=[MinValueValidator(0), MaxValueValidator(900)])
+    weight = models.IntegerField(blank=True, default=0, validators=[MinValueValidator(1), MaxValueValidator(899)])
