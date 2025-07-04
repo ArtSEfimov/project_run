705327d66c00843195fac4490db40d3dd41d6884
Artem <artsefimov@gmail.com>
2025-07-04 23:20:45 +0300

    test Positions Api

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -101,8 +101,8 @@ class FileUploadSerializer(serializers.Serializer):
 # TODO Если при развернутом просмотре User у предмета item не нужен перечень владельцев этого предмета,
 #  надо сделать новый serializer в котором убрать поле users
 
-# class UserDetailSerializer(UserListSerializer):
-#     items = CollectibleItemSerializer(many=True, read_only=True)
-#
-#     class Meta(UserListSerializer.Meta):
-#         fields = UserListSerializer.Meta.fields + ("items",)
+class UserDetailSerializer(UserListSerializer):
+    items = CollectibleItemShowSerializer(many=True, read_only=True)
+
+    class Meta(UserListSerializer.Meta):
+        fields = UserListSerializer.Meta.fields + ("items",)
--- app_run/views/position_views.py
+++ app_run/views/position_views.py
@@ -1,10 +1,11 @@
-
+from functools import cached_property
 
 from django_filters.rest_framework import DjangoFilterBackend
+from haversine import Unit, haversine
 
 from rest_framework.viewsets import ModelViewSet
 
-from ..models import Position
+from ..models import Position, CollectibleItem
 from ..serializers import PositionSerializer
 
 
@@ -14,21 +15,21 @@ class PositionView(ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ["run"]
 
-    # @cached_property
-    # def collectible_items_queryset(self):
-    #     return CollectibleItem.objects.all()
-    #
-    # def perform_create(self, serializer):
-    #     run_instance = serializer.save()
-    #     self.check_nearby_items(run_instance)
-    #
-    # def perform_update(self, serializer):
-    #     run_instance = serializer.save()
-    #     self.check_nearby_items(run_instance)
-    #
-    # def check_nearby_items(self, run_instance):
-    #     for item in self.collectible_items_queryset():
-    #         start = run_instance.latitude, run_instance.longitude
-    #         finish = item.latitude, item.longitude
-    #         if haversine(start, finish, unit=Unit.METERS) <= 100:
-    #             item.users.add(run_instance.athlete)
+    @cached_property
+    def collectible_items_queryset(self):
+        return CollectibleItem.objects.all()
+
+    def perform_create(self, serializer):
+        run_instance = serializer.save()
+        self.check_nearby_items(run_instance)
+
+    def perform_update(self, serializer):
+        run_instance = serializer.save()
+        self.check_nearby_items(run_instance)
+
+    def check_nearby_items(self, run_instance):
+        for item in self.collectible_items_queryset:
+            start = run_instance.latitude, run_instance.longitude
+            finish = item.latitude, item.longitude
+            if haversine(start, finish, unit=Unit.METERS) <= 100:
+                item.users.add(run_instance.athlete)
