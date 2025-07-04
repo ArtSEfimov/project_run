de2d7961560ab2f60860544a161f1d97012e459a
Artem <artsefimov@gmail.com>
2025-07-05 01:26:05 +0300

    fix migrations

--- app_run/models.py
+++ app_run/models.py
@@ -43,4 +43,4 @@ class CollectibleItem(models.Model):
     longitude = models.DecimalField(default=0, decimal_places=4, max_digits=7)
     picture = models.URLField()
     value = models.IntegerField()
-    # users = models.ManyToManyField(User, blank=True, related_name="items", related_query_name="query_items")
+    users = models.ManyToManyField(User, blank=True, related_name="items", related_query_name="query_items")
