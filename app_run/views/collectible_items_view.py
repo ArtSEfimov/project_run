8a2a1311928992e3646d2f44a0eb87f388069eb1
artsefimov <artsefimov@gmail.com>
2025-07-04 08:34:04 +0300

    try new funcs

--- app_run/views/collectible_items_view.py
+++ app_run/views/collectible_items_view.py
@@ -1,10 +1,10 @@
-from openpyxl.reader.excel import load_workbook
-from rest_framework import status
+import openpyxl
+from django.http import JsonResponse
 from rest_framework.generics import ListAPIView
 from rest_framework.parsers import MultiPartParser, FormParser
-from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from validate_url import is_valid_url
 from ..models import CollectibleItem
 from ..serializers import CollectibleItemSerializer, FileUploadSerializer
 
@@ -21,20 +21,66 @@ class UploadFileView(APIView):
         input_serializer = FileUploadSerializer(data=request.data)
         input_serializer.is_valid(raise_exception=True)
         file_object = input_serializer.validated_data["file"]
-        workbook = load_workbook(file_object)
+        # workbook = load_workbook(file_object)
+        # worksheet = workbook.active
+        #
+        # header_row = next(worksheet.iter_rows(min_row=1, max_row=1, values_only=True))
+        # headers = list(h.lower() for h in header_row)
+        #
+        # wrong_rows = list()
+        #
+        # for row in worksheet.iter_rows(min_row=2, values_only=True):
+        #     row_serializer = CollectibleItemSerializer(data=dict(zip(headers, row)))
+        #     if row_serializer.is_valid(raise_exception=False):
+        #         row_serializer.save()
+        #     else:
+        #         print(row_serializer.errors)
+        #         wrong_rows.append(list(row))
+        #
+        # return Response(wrong_rows, status=status.HTTP_200_OK)
+
+        # TRY IT
+
+        # Open the workbook
+        workbook = openpyxl.load_workbook(file_object)
+
+        # Select the active worksheet
         worksheet = workbook.active
 
-        header_row = next(worksheet.iter_rows(min_row=1, max_row=1, values_only=True))
-        headers = list(h.lower() for h in header_row)
+        # Read the contents
+        data = []
+        to_create = []
+        for i, row in enumerate(worksheet.iter_rows(values_only=True)):
+            if i == 0:
+                continue
+            valid = True
+            types = [str, str, int, float, float, str]
+            for index, sub_row in enumerate(row):
+                if type(sub_row) != types[index]:
+                    valid = False
 
-        wrong_rows = list()
+            if not type(row[3]) == float or not (-90 <= row[3] <= 90):
+                valid = False
 
-        for row in worksheet.iter_rows(min_row=2, values_only=True):
-            row_serializer = CollectibleItemSerializer(data=dict(zip(headers, row)))
-            if row_serializer.is_valid(raise_exception=False):
-                row_serializer.save()
+            if not type(row[4]) == float or not (-180 <= row[4] <= 180):
+                valid = False
+
+            if not is_valid_url(row[5]):
+                valid = False
+
+            if not valid:
+                data.append(row)
             else:
-                print(row_serializer.errors)
-                wrong_rows.append(list(row))
+                to_create.append(row)
 
-        return Response(wrong_rows, status=status.HTTP_200_OK)
+        for item in to_create:
+            CollectibleItem.objects.create(name=item[0],
+                                           uid=item[1],
+                                           value=item[2],
+                                           latitude=item[3],
+                                           longitude=item[4],
+                                           picture=item[5],
+                                           )
+
+        # Return the parsed data as JSON
+        return JsonResponse(data, safe=False)
