2b80068718ff4a0f02aff70284187750e3037921
Artem <artsefimov@gmail.com>
2025-07-05 01:04:13 +0300

    fix migrations

--- app_run/views/position_views.py
+++ app_run/views/position_views.py
@@ -15,21 +15,21 @@ class PositionView(ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ["run"]
 
-    # @cached_property
-    # def collectible_items_queryset(self):
-    #     return CollectibleItem.objects.all()
-    #
-    # def perform_create(self, serializer):
-    #     run_instance = serializer.save()
-    #     self.check_nearby_items(run_instance)
-    #
-    # def perform_update(self, serializer):
-    #     run_instance = serializer.save()
-    #     self.check_nearby_items(run_instance)
-    #
-    # def check_nearby_items(self, run_instance):
-    #     for item in self.collectible_items_queryset:
-    #         start = run_instance.latitude, run_instance.longitude
-    #         finish = item.latitude, item.longitude
-    #         if haversine(start, finish, unit=Unit.METERS) <= 100:
-    #             item.users.add(run_instance.athlete)
+    @cached_property
+    def collectible_items_queryset(self):
+        return CollectibleItem.objects.all()
+
+    def perform_create(self, serializer):
+        run_instance = serializer.save()
+        self.check_nearby_items(run_instance)
+
+    def perform_update(self, serializer):
+        run_instance = serializer.save()
+        self.check_nearby_items(run_instance)
+
+    def check_nearby_items(self, run_instance):
+        for item in self.collectible_items_queryset:
+            start = run_instance.latitude, run_instance.longitude
+            finish = item.latitude, item.longitude
+            if haversine(start, finish, unit=Unit.METERS) <= 100:
+                item.users.add(run_instance.athlete)
