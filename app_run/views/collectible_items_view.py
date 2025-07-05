d7131d9956cfc8f9e3095f45ef6f12c2d8c9e0af
Artem <artsefimov@gmail.com>
2025-07-05 12:15:26 +0300

    try serializer validation variant

--- app_run/views/collectible_items_view.py
+++ app_run/views/collectible_items_view.py
@@ -17,95 +17,96 @@ class CollectibleItemView(ListAPIView):
     serializer_class = CollectibleItemShowSerializer
 
 
-# class UploadFileView(APIView):
-#     parser_classes = (MultiPartParser, FormParser)
-#
-#     def post(self, request, *args, **kwargs):
-#         input_serializer = FileUploadSerializer(data=request.data)
-#         input_serializer.is_valid(raise_exception=True)
-#         file_object = input_serializer.validated_data["file"]
-#
-#         workbook = load_workbook(file_object)
-#         worksheet = workbook.active
-#
-#         header_row = next(worksheet.iter_rows(min_row=1, max_row=1, values_only=True))
-#         headers = list(h.lower() for h in header_row if h is not None)
-#
-#         wrong_rows = list()
-#
-#         for row in worksheet.iter_rows(min_row=2, values_only=True):
-#             row_serializer = CollectibleItemSerializer(data=dict(zip(headers, row)))
-#             if row_serializer.is_valid(raise_exception=False):
-#                 row_serializer.save()
-#             else:
-#                 wrong_rows.append(list(row))
-#
-#         return Response(wrong_rows, status=status.HTTP_200_OK)
-
-
-
 class UploadFileView(APIView):
     parser_classes = (MultiPartParser, FormParser)
 
     def post(self, request, *args, **kwargs):
+        input_serializer = FileUploadSerializer(data=request.data)
+        input_serializer.is_valid(raise_exception=True)
+        file_object = input_serializer.validated_data["file"]
 
-        file_object = request.FILES.get('file')
-
-
-        if not file_object:
-            return Response({"error": "No file uploaded"}, status=status.HTTP_400_BAD_REQUEST)
-
-        # Open the workbook
-        workbook = openpyxl.load_workbook(file_object)
-
-        # Select the active worksheet
+        workbook = load_workbook(file_object)
         worksheet = workbook.active
 
-        # Read the contents
-        data = []
-        to_create = []
-        for i, row in enumerate(worksheet.iter_rows(values_only=True)):
-            if i == 0:
-                continue
-            valid = True
-            row = list(row)[:6]
-            try:
-                row[3] = float(row[3])
-                row[4] = float(row[4])
-            except Exception:
-                valid = False
-            types = [str, str, int, float, float, str]
-            for index in range(len(types)):
-                sub_row = row[index]
-                print(sub_row)
-                if type(sub_row) != types[index]:
-                    valid = False
+        # header_row = next(worksheet.iter_rows(min_row=1, max_row=1, values_only=True))
+        # headers = list(h.lower() for h in header_row if h is not None)
+        headers = ["name", "uid", "value", "latitude", "longitude", "url"]
 
-            if not type(row[3]) == float or not (-90 <= row[3] <= 90):
-                valid = False
+        wrong_rows = list()
 
-            if not type(row[4]) == float or not (-180 <= row[4] <= 180):
-                valid = False
-
-            if not validate_url(row[5]):
-                valid = False
-
-            print(row)
-            if not valid:
-                data.append(row)
+        for row in worksheet.iter_rows(min_row=2, values_only=True):
+            row_serializer = CollectibleItemSerializer(data=dict(zip(headers, row)))
+            if row_serializer.is_valid(raise_exception=False):
+                row_serializer.save()
             else:
-                to_create.append(row)
+                wrong_rows.append(list(row))
 
-        for item in to_create:
-            if item[5] is None:
-                continue
-            CollectibleItem.objects.create(name=item[0],
-                                           uid=item[1],
-                                           value=item[2],
-                                           latitude=item[3],
-                                           longitude=item[4],
-                                           picture=item[5],
-                                           )
+        return Response(wrong_rows, status=status.HTTP_200_OK)
 
-        # Return the parsed data as JSON
-        return JsonResponse(data, safe=False)
+
+#
+# class UploadFileView(APIView):
+#     parser_classes = (MultiPartParser, FormParser)
+#
+#     def post(self, request, *args, **kwargs):
+#
+#         file_object = request.FILES.get('file')
+#
+#
+#         if not file_object:
+#             return Response({"error": "No file uploaded"}, status=status.HTTP_400_BAD_REQUEST)
+#
+#         # Open the workbook
+#         workbook = openpyxl.load_workbook(file_object)
+#
+#         # Select the active worksheet
+#         worksheet = workbook.active
+#
+#         # Read the contents
+#         data = []
+#         to_create = []
+#         for i, row in enumerate(worksheet.iter_rows(values_only=True)):
+#             if i == 0:
+#                 continue
+#             valid = True
+#             row = list(row)[:6]
+#             try:
+#                 row[3] = float(row[3])
+#                 row[4] = float(row[4])
+#             except Exception:
+#                 valid = False
+#             types = [str, str, int, float, float, str]
+#             for index in range(len(types)):
+#                 sub_row = row[index]
+#                 print(sub_row)
+#                 if type(sub_row) != types[index]:
+#                     valid = False
+#
+#             if not type(row[3]) == float or not (-90 <= row[3] <= 90):
+#                 valid = False
+#
+#             if not type(row[4]) == float or not (-180 <= row[4] <= 180):
+#                 valid = False
+#
+#             if not validate_url(row[5]):
+#                 valid = False
+#
+#             print(row)
+#             if not valid:
+#                 data.append(row)
+#             else:
+#                 to_create.append(row)
+#
+#         for item in to_create:
+#             if item[5] is None:
+#                 continue
+#             CollectibleItem.objects.create(name=item[0],
+#                                            uid=item[1],
+#                                            value=item[2],
+#                                            latitude=item[3],
+#                                            longitude=item[4],
+#                                            picture=item[5],
+#                                            )
+#
+#         # Return the parsed data as JSON
+#         return JsonResponse(data, safe=False)
